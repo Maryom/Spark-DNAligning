9f6fd479ea5a5f1d25d8aba544d57e7558cbe7e3
Maryom <mrm259@gmail.com>
2019-04-27 17:42:50 +0300

    Added SAM format support

--- src/com/ku/Aligning/DNA.scala
+++ src/com/ku/Aligning/DNA.scala
@@ -6,19 +6,19 @@ import org.apache.spark.mllib.rdd.RDDFunctions._
 import scala.io.Source
 
 object DNA {
-  
+
   /** Our main function where the action happens */
   def main(args: Array[String]) {
-    
+
     /** Load up the DNA reference */
     def load(filename: String) : String = {
 
       val bufferedSource = Source.fromFile(s"$filename")
-    
+
       val ref = bufferedSource.getLines.mkString
 
       bufferedSource.close
-    
+
       return ref
     }
 
@@ -30,46 +30,52 @@ object DNA {
 
     // Create a SparkContext using every core of the cluster
     val sc: SparkContext = new SparkContext("local[*]", "DNA")
-    
+
     val readsLength   = 36
     val partitionsNum = 4
 
     val DNAref = sc.broadcast(load("data/s_suisLine.fa"))
     val DNALength = DNAref.value.length
 
-    val indexing = sc.parallelize(List.range(0, DNALength), partitionsNum).map(index => (DNAref.value.slice(index, index+readsLength), index)).persist
+    val indexing = sc.parallelize(List.range(0, DNALength), partitionsNum).map(index => (DNAref.value.slice(index, index+readsLength), index+1)).persist
 
     val DNAReads = sc.textFile("data/100kGood.fa.bz2", partitionsNum).sliding(2,2).map(IDread => (IDread(1), IDread(0).substring(1))).persist
 
     val indexingPartitioner = new RangePartitioner(partitionsNum, indexing)
     val readsPartitioner    = new RangePartitioner(partitionsNum, DNAReads)
-    
+
     val sortedIndexes = indexing.partitionBy(indexingPartitioner).sortByKey().persist
     val sortedReads   = DNAReads.partitionBy(readsPartitioner).sortByKey().persist
 
     // removing rdd data (cleaning)
     indexing.unpersist()
     DNAReads.unpersist()
-    
+
     List("a","c","g","t").foreach { base =>
-      
+
       val baseRDD   = sortedIndexes.filterByRange(base+"a.*", base+"tt.*").partitionBy(new HashPartitioner(partitionsNum)).persist
       val reads = sortedReads.filterByRange(base+"a.*", base+"tt.*").partitionBy(new HashPartitioner(partitionsNum)).persist
-    
-      val readsResults = reads.join(baseRDD).map(_._2).persist
 
-      readsResults.saveAsTextFile("./output/results"+base)
+      val indexes_readsResults = reads.join(baseRDD).map(index => (index._2._1, index._2._1+"\t0\t"+RNAME+"\t"+index._2._2.toString+"\t60\t"+readsLength.toString+"M\t*\t0\t0\t"+index._1+"\t*")).persist
+
+      // subtractByKey to eliminate the mapped reads
+      val unmapped = reads.map(read_idx => (read_idx._2, read_idx._1)).subtractByKey(indexes_readsResults).map(id_read => id_read._1+"\t4\t*\t0\t0\t*\t*\t0\t0\t"+id_read._2+"\t*").persist
+
+      // save mapped reads
+      indexes_readsResults.values.saveAsTextFile("./output/results"+base)
+      // save unmapped reads
+      unmapped.saveAsTextFile("./unmapped/results"+base)
 
       // removing rdd data (cleaning)
       baseRDD.unpersist()
       reads.unpersist()
-      readsResults.unpersist() 
+      indexes_readsResults.unpersist()
     }
 
     // removing rdd data (cleaning)
     sortedReads.unpersist()
     sortedIndexes.unpersist()
-    
+
     // Stop the timer, as it is a nano second we need to divide it by 1000000000. in 1e9d "d" stands for double
     val duration = (System.nanoTime - t1) / 1e9d
 
--- src/com/ku/Aligning/DNACluster.scala
+++ src/com/ku/Aligning/DNACluster.scala
@@ -6,19 +6,19 @@ import org.apache.spark.mllib.rdd.RDDFunctions._
 import scala.io.Source
 
 object DNACluster {
-  
+
   /** Our main function where the action happens */
   def main(args: Array[String]) {
-    
+
     /** Load up the DNA reference */
     def load(filename: String) : String = {
 
       val bufferedSource = Source.fromFile(s"$filename")
-    
+
       val ref = bufferedSource.getLines.mkString
 
       bufferedSource.close
-    
+
       return ref
     }
 
@@ -34,53 +34,61 @@ object DNACluster {
     conf.set("spark.dynamicAllocation.enabled", "true")
     conf.set("spark.shuffle.service.enabled", "true")
     val sc = new SparkContext(conf)
-    
+
     // read the passing arguments
     val partitions    = args(0)
     val readLength    = args(1)
     val refFile       = args(2)
     val inputFile     = args(3)
     val outputFile    = args(4)
+    val RNAME         = args(5) // reference long name
     val readsLength   = readLength.toInt
     val partitionsNum = partitions.toInt
-    
+
     val DNAref = sc.broadcast(load(refFile))
 
     val DNALength = DNAref.value.length
 
-    val indexing = sc.parallelize(List.range(0, DNALength), partitionsNum).map(index => (DNAref.value.slice(index, index+readsLength), index)).persist
+    val indexing = sc.parallelize(List.range(0, DNALength), partitionsNum).map(index => (DNAref.value.slice(index, index+readsLength), index+1)).persist
 
     val DNAReads = sc.textFile(s"s3n://$inputFile").sliding(2,2).map(IDread => (IDread(1), IDread(0).substring(1))).persist
 
     val indexingPartitioner = new RangePartitioner(partitionsNum, indexing)
     val readsPartitioner    = new RangePartitioner(partitionsNum, DNAReads)
-    
+
     val sortedIndexes = indexing.partitionBy(indexingPartitioner).sortByKey().persist
     val sortedReads   = DNAReads.partitionBy(readsPartitioner).sortByKey().persist
 
     // removing rdd data (cleaning)
     indexing.unpersist()
     DNAReads.unpersist()
-    
+
     List("a","c","g","t").foreach { base =>
-      
+
       val baseRDD   = sortedIndexes.filterByRange(base+"a.*", base+"tt.*").partitionBy(new HashPartitioner(partitionsNum)).persist
       val reads = sortedReads.filterByRange(base+"a.*", base+"tt.*").partitionBy(new HashPartitioner(partitionsNum)).persist
-    
-      val readsResults = reads.join(baseRDD).map(_._2).persist
 
-      readsResults.saveAsTextFile(s"s3://$outputFile"+base)
+      val indexes_readsResults = reads.join(baseRDD).map(index => (index._2._1, index._2._1+"\t0\t"+RNAME+"\t"+index._2._2.toString+"\t60\t"+readsLength.toString+"M\t*\t0\t0\t"+index._1+"\t*")).persist
+
+      // save mapped reads
+      indexes_readsResults.values.saveAsTextFile(s"s3://$outputFile"+base)
+
+      // subtractByKey to eliminate the mapped reads
+      val unmapped = reads.map(read_idx => (read_idx._2, read_idx._1)).subtractByKey(indexes_readsResults).map(id_read => id_read._1+"\t4\t*\t0\t0\t*\t*\t0\t0\t"+id_read._2+"\t*").persist
+
+      // save unmapped reads
+      unmapped.saveAsTextFile(s"s3://$outputFile"+base+"unmapped")
 
       // removing rdd data (cleaning)
       baseRDD.unpersist()
       reads.unpersist()
-      readsResults.unpersist() 
+      indexes_readsResults.unpersist()
     }
-  
+
     // removing rdd data (cleaning)
     sortedReads.unpersist()
     sortedIndexes.unpersist()
-    
+
     // As it is a nano second we need to divide it by 1000000000. in 1e9d "d" stands for double
     val duration = (System.nanoTime - t1) / 1e9d
 
